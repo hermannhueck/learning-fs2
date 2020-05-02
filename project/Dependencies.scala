6231d5ff778fbf7bff9e613b26e6f22af0c0cc01
Hermann Hueck <hermann.hueck@mailbox.org>
2020-05-02 21:30:06 +0200

    update munit: 0.7.1 -> 0.7.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   lazy val fs2Version           = "2.3.0"
   lazy val monixVersion         = "3.2.1"
   lazy val kindProjectorVersion = "0.11.0"
-  lazy val munitVersion         = "0.7.1"
+  lazy val munitVersion         = "0.7.5"
 
   lazy val fs2Core            = "co.fs2"        %% "fs2-core"             % fs2Version
   lazy val fs2Io              = "co.fs2"        %% "fs2-io"               % fs2Version
@@ -13,5 +13,4 @@ object Dependencies {
   lazy val monixEval          = "io.monix"      %% "monix-eval"           % monixVersion
   lazy val munit              = "org.scalameta" %% "munit"                % munitVersion
   lazy val kindProjector      = "org.typelevel" %% "kind-projector"       % kindProjectorVersion cross CrossVersion.full
-
 }
